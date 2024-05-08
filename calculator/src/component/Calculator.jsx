1b87f0d103aafaf5ce91de327cb821396f4b4263
Sambit Sahu <Sambit.Sahu@yovant.com>
2024-05-08 22:54:29 +0530

    Calculator Update Done

---
+++ calculator/src/component/Calculator.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import styles from "../module/Calculator.module.css";
+
+const Calculator = () =>{
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+
+    const handleClick = (value) =>{
+        setInput((prevInput)=>prevInput+value);
+    };
+
+    const handleClear = () =>{
+        setInput('');
+        setOutput('');
+    };
+
+    const handleCalculate = () =>{
+        try{
+            const result = eval(input.replace('x', '*'));
+            setOutput(result);
+        }catch(error){
+            setOutput('Error');
+        }
+    };
+
+    return(
+        <div className={styles.calculator}>
+            <input type="text" className={styles.inputField} value={input} placeholder="Enter expression" readOnly/>
+            <div className={styles.output}>{output}</div>
+            <div className={styles.buttons}>
+                {[7,8,9,'+',4,5,6,'-',1,2,3,'x','C',0,'=','/'].map((item)=>(
+                    <button key={item} className={styles.button}
+                    onClick={()=>{
+                        if(item === '='){handleCalculate();}
+                        else if(item === 'C'){handleClear();}
+                        else{handleClick(item);}
+                    }}
+                    >{item}</button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Calculator;
